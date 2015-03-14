bad67d7a52666d49810f7183fc4755f45d10198a
martysama0134 <martysama0134@gmail.com>
2015-03-14 09:28:56 +0100

    +copyinhere.bat

---
+++ HowToCython/Extra/rootlibpyd/rootlib/copyinhere.bat
@@ -0,0 +1,4 @@
+@copy /v /y "../../../\Main\RootLib\PythonrootlibManager.h" "PythonrootlibManager.h"
+@copy /v /y "../../../\Main\RootLib\PythonrootlibManager.cpp" "PythonrootlibManager.cpp"
+@copy /v /y "../../../\Main\RootLib\cyTemp\rootlib.lib" "rootlib.lib"
+@pause
---
+++ HowToCython/Extra/uiscriptlibpyd/uiscriptlib/copyinhere.bat
@@ -0,0 +1,4 @@
+@copy /v /y "../../../\UiMain\UiScriptLib\PythonuiscriptlibManager.h" "PythonuiscriptlibManager.h"
+@copy /v /y "../../../\UiMain\UiScriptLib\PythonuiscriptlibManager.cpp" "PythonuiscriptlibManager.cpp"
+@copy /v /y "../../../\UiMain\UiScriptLib\cyTemp\uiscriptlib.lib" "uiscriptlib.lib"
+@pause
