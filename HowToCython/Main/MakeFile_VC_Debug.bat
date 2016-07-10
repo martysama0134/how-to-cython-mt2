46b74788f64a8543eca39b06044d838be416458a
martysama0134 <martysama0134@gmail.com>
2016-07-10 19:45:08 +0200

    +removed at from makefile bats

--- HowToCython/Main/MakeFile_VC_Debug.bat
+++ HowToCython/Main/MakeFile_VC_Debug.bat
@@ -1,75 +1,76 @@
-@REM ### Compiler Folder
-@REM ## vs2008 9.0
-@REM @SET VSINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 9.0
-@REM @SET VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC
-@REM ## vs2010 10.0
-@REM @CALL "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
-@SET VSINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 10.0
-@SET VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC
-@REM ## vs2012 11.0
-@REM @CALL "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
-@REM @SET VSINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 11.0
-@REM @SET VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC
-@REM ## vs2013 12.0
-@REM @CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
-@REM @SET VSINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 12.0
-@REM @SET VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC
+@echo off
+REM ### Compiler Folder
+REM ## vs2008 9.0
+REM SET VSINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 9.0
+REM SET VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC
+REM ## vs2010 10.0
+REM CALL "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+SET VSINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 10.0
+SET VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC
+REM ## vs2012 11.0
+REM CALL "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
+REM SET VSINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 11.0
+REM SET VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC
+REM ## vs2013 12.0
+REM CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
+REM SET VSINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 12.0
+REM SET VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC
 
-@REM ### Python Folder
-@SET PYTHONLIBSPATH=C:\Python27\libs
-@SET PYTHONINCLUDEPATH=C:\Python27\include
+REM ### Python Folder
+SET PYTHONLIBSPATH=C:\Python27\libs
+SET PYTHONINCLUDEPATH=C:\Python27\include
 
-@SET FrameworkDir=Framework32
-@SET FrameworkVersion=v2.0.50727
-@if "%VSINSTALLDIR%"=="" goto error_no_VSINSTALLDIR
-@if "%VCINSTALLDIR%"=="" goto error_no_VCINSTALLDIR
+SET FrameworkDir=Framework32
+SET FrameworkVersion=v2.0.50727
+if "%VSINSTALLDIR%"=="" goto error_no_VSINSTALLDIR
+if "%VCINSTALLDIR%"=="" goto error_no_VCINSTALLDIR
 
-@echo Setting environment for using Microsoft Visual Studio 2008 x86 tools.
+echo Setting environment for using Microsoft Visual Studio 2008 x86 tools.
 
-@call :GetWindowsSdkDir
+call :GetWindowsSdkDir
 
-@if not "%WindowsSdkDir%" == "" (
+if not "%WindowsSdkDir%" == "" (
 	set "PATH=%WindowsSdkDir%bin;%PATH%"
 	set "INCLUDE=%WindowsSdkDir%include;%INCLUDE%"
 	set "LIB=%WindowsSdkDir%lib;%LIB%"
 )
 
 
-@rem
-@rem Root of Visual Studio IDE installed files.
-@rem
-@set DevEnvDir=%VSINSTALLDIR%\Common7\IDE
+rem
+rem Root of Visual Studio IDE installed files.
+rem
+set DevEnvDir=%VSINSTALLDIR%\Common7\IDE
 
-@set PATH=%DevEnvDir%;%VCINSTALLDIR%\BIN;%VSINSTALLDIR%\Common7\Tools;%VSINSTALLDIR%\Common7\Tools\bin;%FrameworkDir%\%Framework35Version%;%FrameworkDir%\%Framework35Version%\Microsoft .NET Framework 3.5 (Pre-Release Version);%FrameworkDir%\%FrameworkVersion%;%VCINSTALLDIR%\VCPackages;%PATH%
-@set INCLUDE=%VCINSTALLDIR%\ATLMFC\INCLUDE;%VCINSTALLDIR%\INCLUDE;%INCLUDE%
-@set LIB=%VCINSTALLDIR%\ATLMFC\LIB;%VCINSTALLDIR%\LIB;%LIB%
-@set LIBPATH=%FrameworkDir%\%Framework35Version%;%FrameworkDir%\%FrameworkVersion%;%VCINSTALLDIR%\ATLMFC\LIB;%VCINSTALLDIR%\LIB;%LIBPATH%
+set PATH=%DevEnvDir%;%VCINSTALLDIR%\BIN;%VSINSTALLDIR%\Common7\Tools;%VSINSTALLDIR%\Common7\Tools\bin;%FrameworkDir%\%Framework35Version%;%FrameworkDir%\%Framework35Version%\Microsoft .NET Framework 3.5 (Pre-Release Version);%FrameworkDir%\%FrameworkVersion%;%VCINSTALLDIR%\VCPackages;%PATH%
+set INCLUDE=%VCINSTALLDIR%\ATLMFC\INCLUDE;%VCINSTALLDIR%\INCLUDE;%INCLUDE%
+set LIB=%VCINSTALLDIR%\ATLMFC\LIB;%VCINSTALLDIR%\LIB;%LIB%
+set LIBPATH=%FrameworkDir%\%Framework35Version%;%FrameworkDir%\%FrameworkVersion%;%VCINSTALLDIR%\ATLMFC\LIB;%VCINSTALLDIR%\LIB;%LIBPATH%
 
-@goto end
+goto end
 
 :GetWindowsSdkDir
-@call :GetWindowsSdkDirHelper HKLM > nul 2>&1
-@if errorlevel 1 call :GetWindowsSdkDirHelper HKCU > nul 2>&1
-@if errorlevel 1 set WindowsSdkDir=%VCINSTALLDIR%\PlatformSDK\
-@exit /B 0
+call :GetWindowsSdkDirHelper HKLM > nul 2>&1
+if errorlevel 1 call :GetWindowsSdkDirHelper HKCU > nul 2>&1
+if errorlevel 1 set WindowsSdkDir=%VCINSTALLDIR%\PlatformSDK\
+exit /B 0
 
 :GetWindowsSdkDirHelper
-@SET WindowsSdkDir=
-@for /F "tokens=1,2*" %%i in ('reg query "%1\SOFTWARE\Microsoft\Microsoft SDKs\Windows" /v "CurrentInstallFolder"') DO (
+SET WindowsSdkDir=
+for /F "tokens=1,2*" %%i in ('reg query "%1\SOFTWARE\Microsoft\Microsoft SDKs\Windows" /v "CurrentInstallFolder"') DO (
 	if "%%i"=="CurrentInstallFolder" (
 		SET "WindowsSdkDir=%%k"
 	)
 )
-@if "%WindowsSdkDir%"=="" exit /B 1
-@exit /B 0
+if "%WindowsSdkDir%"=="" exit /B 1
+exit /B 0
 
 :error_no_VSINSTALLDIR
-@echo ERROR: VSINSTALLDIR variable is not set.
-@goto end
+echo ERROR: VSINSTALLDIR variable is not set.
+goto end
 
 :error_no_VCINSTALLDIR
-@echo ERROR: VCINSTALLDIR variable is not set.
-@goto end
+echo ERROR: VCINSTALLDIR variable is not set.
+goto end
 
 :end
 
