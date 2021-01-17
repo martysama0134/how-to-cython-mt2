76aa2920926298c916174f73a7c8a008e4a8b726
Marty Sama <martysama0134@gmail.com>
2021-01-17 16:32:59 +0100

    CRootLib clear unused files

--- HowToCython/CRootLib/Workplace/4_CompileCython.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-copy /y MakeFile_VC_Release.bat RootLib\cyTemp\
-cd RootLib\cyTemp\
-call MakeFile_VC_Release.bat
-pause
--- HowToCython/CRootLib/Workplace/5_CopyLibFromCythonFolder.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-mkdir out
-copy /y RootLib\PythonrootlibManager.cpp out\
-copy /y RootLib\PythonrootlibManager.h out\
-copy /y RootLib\cyTemp\rootlib.lib out\
-echo copy completed
-pause
